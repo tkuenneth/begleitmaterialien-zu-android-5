30f169d2e21d4fa9299bad6d320a143de6bb46e8
Thomas Kuenneth <kuennetht@gmail.com>
2018-12-13 09:14:21 +0100

    updated to Android Pie

--- Missed Calls Widget/app/src/main/java/com/thomaskuenneth/missedcalls/MissedCallsService.java
+++ Missed Calls Widget/app/src/main/java/com/thomaskuenneth/missedcalls/MissedCallsService.java
@@ -1,8 +1,11 @@
 package com.thomaskuenneth.missedcalls;
 
+import android.Manifest;
 import android.app.Service;
+import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.os.Handler;
@@ -13,9 +16,6 @@ import android.util.Log;
 
 public class MissedCallsService extends Service {
 
-    public static final String ACTION_MISSED_CALL = "com.thomaskuenneth.missedcalls.MISSED_CALLS";
-    public static final String EXTRA_MISSED_CALL = "MISSED_CALLS";
-
     private static final String TAG = MissedCallsService.class.getSimpleName();
 
     private ContentObserver contentObserver;
@@ -24,7 +24,6 @@ public class MissedCallsService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG, "onBind()");
-        // Service wird nicht mit bindService() gebunden
         return null;
     }
 
@@ -32,45 +31,78 @@ public class MissedCallsService extends Service {
     public void onCreate() {
         Log.d(TAG, "onCreate()");
         lastMissedCalls = -1;
-        contentObserver = new ContentObserver(new Handler()) {
-            @Override
-            public void onChange(boolean selfChange) {
-                int missedCalls = getMissedCalls(MissedCallsService.this);
-                Log.d(TAG, "onChange() - " + missedCalls + " entgangene Anrufe");
-                if (missedCalls != lastMissedCalls) {
-                    lastMissedCalls = missedCalls;
-                    // Intent erstellen und senden
-                    Intent i = new Intent(ACTION_MISSED_CALL);
-                    i.putExtra(EXTRA_MISSED_CALL, missedCalls);
-                    sendBroadcast(i);
-                }
-            }
-        };
-        getContentResolver().registerContentObserver(CallLog.Calls.CONTENT_URI,
-                false, contentObserver);
+        contentObserver = null;
     }
 
     @Override
     public void onDestroy() {
         Log.d(TAG, "onDestroy()");
-        getContentResolver().unregisterContentObserver(contentObserver);
+        if (canReadCallLog(this)) {
+            if (contentObserver != null) {
+                getContentResolver().unregisterContentObserver(contentObserver);
+            }
+        }
         contentObserver = null;
         lastMissedCalls = -1;
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        if (canReadCallLog(this)) {
+            if (contentObserver == null) {
+                contentObserver = new ContentObserver(new Handler()) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        int missedCalls = getMissedCalls(MissedCallsService.this);
+                        Log.d(TAG, "onChange() - " + missedCalls + " entgangene Anrufe");
+                        if (missedCalls != lastMissedCalls) {
+                            lastMissedCalls = missedCalls;
+                            // Intent erstellen und senden
+                            Intent i = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                            sendBroadcast(i);
+                        }
+                    }
+                };
+                getContentResolver().registerContentObserver(CallLog.Calls.CONTENT_URI,
+                        false, contentObserver);
+            }
+        }
+        return START_STICKY;
+    }
+
     public static int getMissedCalls(Context context) {
         int missedCalls = -1;
-        String[] projection = {Calls._ID};
-        String selection = Calls.TYPE + " = ?";
-        String[] selectionArgs = {Integer.toString(Calls.MISSED_TYPE)};
-        Cursor c = context.getContentResolver().query(
-                CallLog.Calls.CONTENT_URI, projection, selection,
-                selectionArgs, null);
-        if (c != null) {
-            missedCalls = c.getCount();
-            c.close();
+        if (canReadCallLog(context)) {
+            String[] projection = {Calls._ID};
+            String selection = Calls.TYPE + " = ?";
+            String[] selectionArgs = {Integer.toString(Calls.MISSED_TYPE)};
+            Cursor c = context.getContentResolver().query(
+                    CallLog.Calls.CONTENT_URI, projection, selection,
+                    selectionArgs, null);
+            if (c != null) {
+                missedCalls = c.getCount();
+                c.close();
+            }
+            Log.d(TAG, "getMissedCalls() - " + missedCalls + " entgangene Anrufe");
         }
-        Log.d(TAG, "getMissedCalls() - " + missedCalls + " entgangene Anrufe");
         return missedCalls;
+    }
+
+    public static boolean canReadCallLog(Context context) {
+        return context.checkSelfPermission(Manifest.permission.READ_CALL_LOG)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static void startService(Context context) {
+        if (MissedCallsService.canReadCallLog(context)) {
+            Intent intent = new Intent(context, MissedCallsService.class);
+            context.startService(intent);
+        }
+    }
+
+    public static void stopService(Context context) {
+        Intent intent = new Intent(context, MissedCallsService.class);
+        context.stopService(intent);
     }
 }
--- Missed Calls Widget/app/src/main/java/com/thomaskuenneth/missedcalls/MissedCallsWidget.java
+++ Missed Calls Widget/app/src/main/java/com/thomaskuenneth/missedcalls/MissedCallsWidget.java
@@ -3,97 +3,92 @@ package com.thomaskuenneth.missedcalls;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.provider.CallLog;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 public class MissedCallsWidget extends AppWidgetProvider {
 
-    public static final String BACKGROUND_COLOR = "BC_";
     public static final int COLOR_SEMI_TRANSPARENT = 0x80000000;
     public static final String MCW = "MissedCallsWidget";
 
     private static final String TAG = MissedCallsWidget.class.getSimpleName();
+    private static final String BC = "BC_";
 
     @Override
     public void onEnabled(Context context) {
         Log.d(TAG, "onEnabled()");
-        super.onEnabled(context);
-        Intent service = new Intent(context, MissedCallsService.class);
-        context.startService(service);
+        MissedCallsService.startService(context);
     }
 
     @Override
     public void onDisabled(Context context) {
         Log.d(TAG, "onDisabled()");
-        super.onDisabled(context);
-        Intent service = new Intent(context, MissedCallsService.class);
-        context.stopService(service);
-    }
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "onReceive()");
-        super.onReceive(context, intent);
-        if (MissedCallsService.ACTION_MISSED_CALL
-                .equals(intent.getAction())) {
-            int missedCalls = intent.getIntExtra(
-                    MissedCallsService.EXTRA_MISSED_CALL, -1);
-            if (missedCalls != -1) {
-                AppWidgetManager m = AppWidgetManager.getInstance(context);
-                int[] appWidgetIds = m
-                        .getAppWidgetIds(new ComponentName(context,
-                                getClass()));
-                updateWidgets(context, m, appWidgetIds, missedCalls);
-            }
-        }
+        MissedCallsService.stopService(context);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
-        updateWidgets(context, appWidgetManager, appWidgetIds,
-                MissedCallsService.getMissedCalls(context));
+        SharedPreferences prefs = context.getSharedPreferences(MCW, Context.MODE_PRIVATE);
+        for (int id : appWidgetIds) {
+            final RemoteViews views = getRemoteViews(context, prefs, id);
+            appWidgetManager.updateAppWidget(id, views);
+        }
     }
 
     @Override
     public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
-        super.onRestored(context, oldWidgetIds, newWidgetIds);
         SharedPreferences prefs = context.getSharedPreferences(MCW, Context.MODE_PRIVATE);
         SharedPreferences.Editor e = prefs.edit();
         for (int i = 0; i < oldWidgetIds.length; i++) {
-            int color = prefs.getInt(BACKGROUND_COLOR + Integer.toString(oldWidgetIds[i]), COLOR_SEMI_TRANSPARENT);
-            e.putInt(BACKGROUND_COLOR + Integer.toString(newWidgetIds[i]), color);
+            String oldWidgetKey = getColorKey(oldWidgetIds[i]);
+            int color = prefs.getInt(oldWidgetKey, COLOR_SEMI_TRANSPARENT);
+            e.remove(oldWidgetKey);
+            e.putInt(getColorKey(newWidgetIds[i]), color);
         }
         e.apply();
     }
 
-    private void updateWidgets(Context context,
-                               AppWidgetManager appWidgetManager, int[] appWidgetIds,
-                               int missedCalls) {
-        SharedPreferences prefs = context.getSharedPreferences(MCW, Context.MODE_PRIVATE);
-        for (int id : appWidgetIds) {
-            int color = prefs.getInt(BACKGROUND_COLOR + Integer.toString(id), COLOR_SEMI_TRANSPARENT);
-            // Layoutdatei entfalten
-            final RemoteViews updateViews = new RemoteViews(
-                    context.getPackageName(), R.layout.missedcallswidget_layout);
-            // Farbe setzen
-            updateViews.setInt(R.id.background, "setBackgroundColor", color);
-            // Zahl entgangener Anrufe ausgeben
-            updateViews.setTextViewText(R.id.missedcallswidget_count,
-                    Integer.toString(missedCalls));
-            // auf Antippen reagieren
-            Intent intent = new Intent(Intent.ACTION_VIEW,
-                    CallLog.Calls.CONTENT_URI);
-            intent.setType(CallLog.Calls.CONTENT_TYPE);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-                    intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            updateViews.setOnClickPendingIntent(R.id.widget_id, pendingIntent);
-            appWidgetManager.updateAppWidget(id, updateViews);
+    public static RemoteViews getRemoteViews(Context context, SharedPreferences prefs, int id) {
+        int color = getColor(prefs, id);
+        Log.d(TAG, "getRemoteViews(): " + id);
+        boolean canRead = MissedCallsService.canReadCallLog(context);
+        final RemoteViews views = new RemoteViews(context.getPackageName(),
+                canRead ? R.layout.missedcallswidget_layout : R.layout.widget_initial_layout);
+        views.setInt(R.id.background, "setBackgroundColor", color);
+        // Konfigurations-Activity
+        Intent configIntent = new Intent(context, MissedCallsConfigActivity.class);
+        configIntent.setData(Uri.withAppendedPath(Uri.parse("abc://widget/id/"), String.valueOf(id)));
+        configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id);
+        PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0,
+                configIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Call Log
+        Intent callLogIntent = new Intent(Intent.ACTION_VIEW);
+        callLogIntent.setDataAndType(CallLog.Calls.CONTENT_URI, CallLog.Calls.CONTENT_TYPE);
+        PendingIntent callLogPendingIntent = PendingIntent.getActivity(context, 0,
+                callLogIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        if (canRead) {
+            views.setOnClickPendingIntent(R.id.config, configPendingIntent);
+            views.setOnClickPendingIntent(R.id.background, callLogPendingIntent);
+            views.setTextViewText(R.id.missedcallswidget_count,
+                    Integer.toString(MissedCallsService.getMissedCalls(context)));
+        } else {
+            views.setTextViewText(R.id.tv, context.getString(R.string.missing_permission));
+            views.setOnClickPendingIntent(R.id.background, configPendingIntent);
         }
+        return views;
+    }
+
+    public static String getColorKey(int id) {
+        return BC + Integer.toString(id);
+    }
+
+    private static int getColor(SharedPreferences prefs, int id) {
+        return prefs.getInt(getColorKey(id), COLOR_SEMI_TRANSPARENT);
     }
 }
